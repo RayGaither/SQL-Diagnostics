fb0d0cf19526d05db41145b33339252814a0a3ed
Ray Gaither <raygaitherjr@yahoo.com>
2023-04-14 10:30:46 -0400

    saving latest changes from testing

--- Diagnostic.v1.5.ps1
+++ Diagnostic.v1.5.ps1
@@ -6,11 +6,11 @@
 # Diag files will be script name with no spaces or file extentions
 
 # create boolean set to true to create the diag files once.  All servers in list are same version
-$createDiagScripts = $true
-
+$createDiagScripts = $False	
+# $wincred = Get-Credential
 # Set path and file name for server/instance list
 $listPath = 'C:\Users\argaither\HubbellHealthCheck'
-$listFile = 'SQL2019InstanceList.txt'
+$listFile = 'SQL2016InstanceList.txt'
 
 # Open server/instance list and loop through them
 $instanceList = get-content -path "$($listPath)\$($listFile)"
@@ -18,34 +18,54 @@ $instanceList = get-content -path "$($listPath)\$($listFile)"
 # Loop through each server instance
 ForEach ($Instance in $instanceList) {
 	# connect to instance
-	try {
+	 try {
+			# $serverConn | Disconnect-DbaInstance
+			
 			$serverConn = Connect-DbaInstance -SqlInstance $Instance -TrustServerCertificate
-			if($createDiagScripts) {
-				$createDiagScripts = $False
-				# delete folder content before creating new scripts
-				$queryOutputPath = 'C:\Users\argaither\Documents\Diag\Instance_Diags' #  <--- is the path correct
-				# instance only
-				Invoke-DbaDiagnosticQuery -SqlInstance $serverConn -ExportQueries -OutputPath $queryOutputPath -InstanceOnly
-			} # end if
-			# use instance name for workbook name
-			$resultsOutputPath = "$($queryOutputPath)\results\$($Instance)_diag.xlsx"  
-				 
-			# loop through diagnostic scripts
-			$scriptList = Get-ChildItem "$($queryOutputPath)\*.sql"
-			foreach ($script in $scriptList) {
-				# Clean File Name for Excel output of Worksheet and Table Name
-				# Remove invalid Characters, set Pascal Case
-				$name = $script.BaseName -replace "(?:^|_|-|\s|\s-\s)(\p{L}) ", '' #{ $_.Groups.Value.ToUpper() }
-				$tblname = $name -replace " ", ''
-				if ($name.Length -gt 25) {$name = $name.Substring(0,25)}
+			if ($null -eq $serverConn) {
 				
-				# write diagnostic results to excel tab
-				Invoke-DbaQuery -SqlInstance $serverConn -File $tblname | Export-CSV -Path $resultsOutputPath -NoTypeInformation -UseQuotes AsNeeded
-			} # end Diag Script Loop
-		}
+				if($createDiagScripts) {
+					$createDiagScripts = $False
+					# delete folder content before creating new scripts
+					$queryOutputPath = 'C:\Users\argaither\Documents\Diag\Instance_Diags' #  <--- is the path correct
+					# instance only
+					Invoke-DbaDiagnosticQuery -SqlInstance $serverConn -ExportQueries -OutputPath $queryOutputPath -InstanceOnly
+				} # end if
+				
+				# use instance name for workbook name
+				$resultsOutputPath = "$($queryOutputPath)\results\$($Instance)_diag.xlsx"  
+					
+				# loop through diagnostic scripts
+				$scriptList = Get-ChildItem "$($queryOutputPath)\*.sql"
+				foreach ($script in $scriptList) {
+					# Clean File Name for Excel output of Worksheet and Table Name
+					# Remove invalid Characters, set Pascal Case
+					$fn = $script
+					$name = $script.BaseName -replace "(?:^|_|-|\s|\s-\s)(\p{L}) ", '' #{ $_.Groups.Value.ToUpper() }
+					$tblname = $name -replace " ", ''
+					if ($name.Length -gt 25) {$name = $name.Substring(0,25)}
+					
+					# write diagnostic results to excel tab
+					Invoke-DbaQuery -SqlInstance $serverConn -File $fn | Export-Excel -Path $resultsOutputPath -TableName $tblname -WorksheetName $tblname -AutoSize -AutoFilter -ClearSheet
+				} # end Diag Script Loop
+				Write-Output "1 Not able to connect to: $($Instance) "
+			} # end connect if test	and go to next instance
+		} # end try
 		catch {
-				Write-Output "Not able to connect to: $($Instance) "
+				Write-Output "2 Not able to connect to: $($Instance) "
+                
 		} # End catch
 		
 	}   # INSTANCE LOOP END                       
-					
+	#	Install-Module -Name ImportExcel
+
+    # Get-module ImportExcel			
+	# Get-DbaConnectedInstance | Select *
+    # Connect-DbaInstance
+    # Disconnect-DbaInstance | UseasTeamMateVM
+    # Get-DbaConnectedInstance
+    # Get-DbaConnection
+    # Test-DbaConnection -sqlinstance UseasTeamMateVM
+    # winrm help config
+
+    # Get-DbaConnectedInstance | Disconnect-DbaInstance
